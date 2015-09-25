6925f7041c6488a56be28e0f446f500827dc846c
Asgeir S. Hustad <askhus@online.no>
2015-09-25 13:37:13 +0000

    Fjernet rediger-knapp for kommuner.

--- controller/form.controller.php
+++ controller/form.controller.php
@@ -4,19 +4,12 @@ if ($_GET)
 $pl = new monstring(get_option("pl_id"));
 $fylke = $pl->get('fylke_id');
 
-
 // Finn event-id fra GET-variabel
 $event_id = $_GET['id'];
-var_dump($fylke);
-var_dump($event_id);
-echo "<br />";
+
 // Kjør SQL-query for å finne data om eventen
 $sql = new SQL("SELECT * FROM `ukm_kalender` WHERE `id` = ".$event_id." LIMIT 1");
 $res = $sql->run();
-
-var_dump($sql);
-echo "<br />";
-var_dump($res);
 
 if( $res ) {
 	$row = mysql_fetch_assoc( $res );
@@ -25,8 +18,6 @@ if( $res ) {
 	$row['location'] = utf8_encode($row['location']);
 
 	$INFOS['editevent'] = $row;
-
-	
 }
 
 ?>
